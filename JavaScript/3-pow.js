cee6cd87d63307945cd38832dc14154475bd7178
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2022-08-23 10:54:54 +0300

    Fix power

--- JavaScript/3-pow.js
+++ JavaScript/3-pow.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const pow = (base, power) => {
-  if (power === 1) return base;
-  else return pow(base, power - 1) * base;
+  if (power === 0) return 1;
+  return pow(base, power - 1) * base;
 };
 
 console.log(pow(2, 3));
