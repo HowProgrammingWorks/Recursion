f4302a3d5f3c5b08c8923ad5ac233136b6699040
thebladehit <boqdanyarmolka@gmail.com>
2022-11-07 15:18:45 +0200

    Add an Exercises folder. Add autotest

---
+++ Exercises/1-showelem.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const deepSum = (array) => {
+  // use recursion to sum all element of array
+  // deepSum([1, [2, 3, [4, [5]]], 6, [33]]) = 54
+};
+
+module.exports = { deepSum };
---
+++ Exercises/2-powloop.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const pow = (base, power) => {
+  // use loop to raise a number to a power
+  // do not use recursion
+};
+
+module.exports = { pow };
